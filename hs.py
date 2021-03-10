6123ec5cd4bb343d0e43bfb2463c61bae32a7825
anthony0625 <anthonykntk@gmail.com>
2021-03-10 23:06:01 +0900

    init function

--- hs.py
+++ hs.py
@@ -1,1 +1,10 @@
 class CellObj(object):
+	def __init__(self):
+		self.x = self.y = self.z = 0
+		self.create_sections()
+		self.build_topology()
+		self.build_subsets()
+		self.define_geometry()
+		self.define_biophysics()
+
+end
